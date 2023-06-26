a920278a587208b1b192b7dfb4698fa246927ad5
pavelmaks <pavel651@yandex.ru>
2023-06-27 01:29:28 +0300

    Fix bug: db start earlier django app

--- app/uwsgi/run_uwsgi.sh
+++ app/uwsgi/run_uwsgi.sh
@@ -5,10 +5,10 @@ apt-get update
 apt-get -y install gettext
 python manage.py makemessages -l en -l ru
 python manage.py compilemessages -l en -l ru
+/opt/app/uwsgi/wait-for-it.sh $DB_HOST:5432 --strict --timeout=30
 
-if [ "$DEBUG" != 'True' ]; then
+if [ "$DEBUG" != 'True' ]; then 
     uwsgi --strict --ini /opt/app/uwsgi/uwsgi.ini
 else
     python manage.py runserver service:8000
 fi
-# python manage.py runserver service:8000
